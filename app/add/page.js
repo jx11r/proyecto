ddcfd29cbcad69cbdc3a7b9ae87a6a0ce084c62f
jx11r <jx11r@hotmail.com>
2024-12-08 13:18:28 -0600

    refactor: rename route

--- app/add/page.js
+++ app/add/page.js
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 
-export default function NewTransaction() {
+export default function AddTransaction() {
   return (
     <main>
       <section className={styles["add-transaction"]}>
