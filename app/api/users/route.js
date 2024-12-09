9e355ccf2fcbd750a98458dfcd62c1f48db61d19
jx11r <jx11r@hotmail.com>
2024-12-08 20:25:04 -0600

    refactor(api): clean up routes

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -2,13 +2,6 @@
 
 import bcrypt from "bcrypt";
 import { query } from "@/lib/db";
-import { withAuth } from "@/lib/dal";
-
-export async function GET() {
-  return withAuth(async () => {
-    return query("SELECT * FROM users", [], 200);
-  });
-}
 
 export async function POST(request) {
   const data = await request.json();
@@ -16,12 +9,4 @@ export async function POST(request) {
   const sql = "INSERT INTO users VALUES (?, ?)";
   const { username, _ } = data;
   return query(sql, [username, hashedPassword], 201);
-}
-
-export async function DELETE(request) {
-  return withAuth(async () => {
-    const data = await request.json();
-    const sql = "DELETE FROM users WHERE username = ?";
-    return query(sql, [data.username], 204);
-  });
 }
